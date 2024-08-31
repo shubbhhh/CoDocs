5bf26cef178d31c93a5b3d6cbea3b64439be276e
svy-exe <shubhamvy07@gmail.com>
2024-08-31 10:38:18 +0530

    � Added auth using Clerk

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,12 +1,22 @@
 import Header from "@/components/Header";
+import {
+    SignInButton,
+    SignedIn,
+    SignedOut,
+    UserButton
+  } from '@clerk/nextjs'
 
 const Document = () => {
     return (
         <div>
             <Header>
-                hi
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </Header>
-            Doc
         </div>
     )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,29 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Header = ({ children }: HeaderProps) => {
+const Header = ({ children, className }: HeaderProps) => {
     return (
-        <div>
-            <Link href="/" className="md:flex-1">
-                <Image 
-                    src="/assets/icons/logo.svg"
-                    alt="Logo"
-                    width={120}
-                    height={32}
-                    className="hidden md:block"
-                />
-            </Link>
-        </div>
+    <div className={cn("header", className)}>
+      <Link href='/' className="md:flex-1">
+        <Image 
+          src="/assets/icons/logo.svg"
+          alt="Logo with name"
+          width={120}
+          height={32}
+          className="hidden md:block"
+        />
+        <Image 
+          src="/assets/icons/logo-icon.svg"
+          alt="Logo"
+          width={32}
+          height={32}
+          className="mr-2 md:hidden"
+        />
+      </Link>
+      {children}
+    </div>
     )
 }
 
