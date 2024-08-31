350f7d3c7fc6f796f3752e033fe4c42ef763e4c9
svy-exe <shubhamvy07@gmail.com>
2024-08-31 12:46:58 +0530

    :pencil: Added text editor

---
+++ components/Editor/Editor.tsx
@@ -0,0 +1,56 @@
+'use client';
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
+import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary';
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
+
+import ExampleTheme from './plugins/Theme';
+import ToolbarPlugin from './plugins/ToolbarPlugin';
+
+const placeholder = 'Enter some rich text...';
+
+const editorConfig = {
+  namespace: 'Demo',
+  nodes: [],
+  // Handling of errors during update
+  onError(error: Error) {
+    throw error;
+  },
+  // The editor theme
+  theme: ExampleTheme,
+};
+
+export function Editor() {
+  return (
+    <LexicalComposer initialConfig={editorConfig}>
+      <div className="editor-container">
+        <ToolbarPlugin />
+        <div className="editor-inner">
+          <RichTextPlugin
+            contentEditable={
+              <ContentEditable
+                className="editor-input"
+                aria-placeholder={placeholder}
+                placeholder={
+                  <div className="editor-placeholder">{placeholder}</div>
+                }
+              />
+            }
+            ErrorBoundary={LexicalErrorBoundary}
+          />
+          <HistoryPlugin />
+          <AutoFocusPlugin />
+        </div>
+      </div>
+    </LexicalComposer>
+  );
+}
