12de997ab5f2d5d46f34cd417c15d37ca8e82a8a
svy-exe <shubhamvy07@gmail.com>
2024-09-07 20:49:27 +0530

    :pencil: home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import Image from "next/image";
-import Editor from "@/components/Editor";
+import { RecentDocs } from "@/components/RecentDoc";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-      <Editor />
+      <div>
+        <Header />
+      </div>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+import { Search } from "lucide-react"
+
+export default function Header() {
+    return (
+        <div className="p-2 shadow flex justify-between">
+            <div className="flex items-center">
+                <img src="/docsIcon.png" alt="Doc Icon" height={40} width={40} />
+                <div className="font-semibold items-center">
+                    Co-Docs
+                </div>
+            </div>
+
+            <div className="flex border px-2 rounded-md w-1/4 items-center">
+                <Search />
+                <input type="text" placeholder="Search" name="" id="" className="px-2 outline-none" />
+            </div>
+
+            <div className="bg-black h-10 w-10 rounded-full">
+
+            </div>
+        </div>
+    )
+}
