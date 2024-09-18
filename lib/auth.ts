69b9f08ea81344a79392b1eb1518766c309f9098
svy-exe <shubhamvy07@gmail.com>
2024-09-18 20:54:20 +0530

    auth

---
+++ lib/auth.ts
@@ -0,0 +1,35 @@
+import { Adapter } from "next-auth/adapters";
+// import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const authProvisers = {
+    // adapter: PrismaAdapter(prisma) as Adapter,
+    providers: [
+        CredentialsProvider({
+            name: "Email",
+            credentials: {
+                username: { label: "Email", type: "text", placeholder: "Email" },
+                password: { label: "Password", type: "password", placeholder: "Password" }
+            },
+            async authorize(credentials, req) {
+                return {
+                    username: credentials?.username,
+                    id: "user1"
+                }
+            },
+        }),
+        GoogleProvider({
+            clientId: process.env.CLIENT_ID ?? '',
+            clientSecret: process.env.CLIENT_SECRET ?? '',
+        }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+  
+    // Uncomment the following lines when add custom signin page added
+    // pages: {
+    //   signIn: '/signin',
+    // }
+};
+
+export default authProvisers;
